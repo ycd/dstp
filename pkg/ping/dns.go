887150b978bf9fa687a76f71f5587d54486a455b
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-11-20 19:18:59 +0300

    feat(ping): iplement fallback for *nix
    Signed-off-by: Yagiz Degirmenci <yagizcanilbey1903@gmail.com>

---
+++ pkg/ping/dns.go
@@ -0,0 +1,31 @@
+package ping
+
+import (
+	"context"
+	"fmt"
+	"github.com/ycd/dstp/pkg/common"
+	"time"
+)
+
+func RunDNSTest(ctx context.Context, addr common.Address, count int, timeout int) (common.Output, error) {
+	var output string
+
+	pinger, err := createPinger(addr.String())
+	if err != nil {
+		return "", err
+	}
+
+	pinger.Count = count
+	if timeout == -1 {
+		pinger.Timeout = time.Duration(10*count) * time.Second
+	} else {
+		pinger.Timeout = time.Duration(timeout) * time.Second
+	}
+	err = pinger.Run()
+	if err != nil {
+		return "", fmt.Errorf("failed to run ping: %v", err.Error())
+	}
+
+	output += joinS("resolving", pinger.IPAddr().String())
+	return common.Output(output), nil
+}
