795acb82ed6c360e2c37c66acea5f4e5606fe22e
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-11-22 23:37:57 +0300

    chore(lookup): update tests with the new api
    Signed-off-by: Yagiz Degirmenci <yagizcanilbey1903@gmail.com>

--- pkg/lookup/host_test.go
+++ pkg/lookup/host_test.go
@@ -12,10 +12,12 @@ import (
 func TestLookup(t *testing.T) {
 	var wg sync.WaitGroup
 	var result common.Result
+	wg.Add(1)
 	err := Host(context.Background(), &wg, "jvns.ca", &result)
 	if err != nil {
 		t.Fatal(err)
 	}
+	wg.Wait()
 
 	if result.SystemDNS == "" {
 		t.Fatal(err)
